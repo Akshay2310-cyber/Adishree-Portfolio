40ada9c41151eda0363fd4062bfad162eabab8b4
Akshay2310-cyber <ms.akshay.2310@gmail.com>
2025-11-07 15:06:55 +0530

    vercel prod crctns

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -11,14 +11,14 @@ const Page = () => {
           <LinkPreview url="https://rvu.edu.in/" className="font-bold">
             {" "}
             RV University
-          </LinkPreview>, I’ve been actively exploring the real-world impact of AI across applications—from building Flutter-based mobile solutions to developing intelligent systems during my internships. I’ve worked as a Machine Learning Intern at EY, where I contributed to building Agentic AI frameworks for Natural Language to SQL workflows, and I’m currently an AI Application Developer Intern at ATmega Software Technologies LLP.
+          </LinkPreview>, I have been actively exploring the real-world impact of AI across applications—from building Flutter-based mobile solutions to developing intelligent systems during my internships. I have worked as a Machine Learning Intern at EY, where I contributed to building Agentic AI frameworks for Natural Language to SQL workflows, and I am currently an AI Application Developer Intern at ATmega Software Technologies LLP.
         </p>
         <div className="mt-4">
           Previously, I interned at Shaale as a Flutter Developer, where I built mobile features using Dart and Flutter.
 My technical interests span AI Agents, RAG Pipelines, LangChain, Machine Learning, NLP, Data Engineering, and Generative AI. I enjoy creating data-driven solutions that are practical, scalable, and intelligent.
         </div>
         <p className="mt-4">
-          Alongside academics and projects, I’ve also taken part in hackathons, university tech events, and collaborative problem-solving with teams. I'm currently seeking a 6-month internship in Data Science, ML, AI, or Data Engineering where I can apply my skills and continue learning in real-world environments.
+          Alongside academics and projects, I have also taken part in hackathons, university tech events, and collaborative problem-solving with teams. I am currently seeking a 6-month internship in Data Science, ML, AI, or Data Engineering where I can apply my skills and continue learning in real-world environments.
         </p>
       </div>
     </section>
